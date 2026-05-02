d0bc47250596ba2ef28e0f280ee57d47f8194154
Abiha439 <syedaabiha07@gmail.com>
2026-05-02 16:03:29 +0500

    DSA Day8

---
+++ DSA/724. Find pivot index.cpp
@@ -0,0 +1,22 @@
+                       // Prefix Sum (Pattern)
+
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = 0;
+         for (int num : nums) total += num;  
+      int leftSum = 0;
+       for (int i = 0; i < nums.size(); i++) {
+       int rightSum = total - leftSum - nums[i];
+        if (leftSum == rightSum) {
+            return i;
+        }
+            leftSum += nums[i];
+       }
+        return -1; 
+    }
+};
+
+
+Time Complexity: 0(n)
+Space Complexity: 0(1)
