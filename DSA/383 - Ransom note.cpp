429304716a20ee90164287739be91e2f8d4e44e6
Abiha439 <syedaabiha07@gmail.com>
2026-05-17 23:19:35 +0500

    DSA Day 18

---
+++ DSA/383 - Ransom note.cpp
@@ -0,0 +1,18 @@
+                        // Hash Map Frequency Counting
+
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> freq;
+        for (char c : magazine) freq[c]++;
+        for (char c : ransomNote) {
+            if (--freq[c] < 0) return false;
+        }
+        return true;
+    }
+};
+
+
+Time Complexity: O(m + n)
+Space Complexity: O(1)
+
