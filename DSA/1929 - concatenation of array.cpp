728f1b6dd3024acb7f0c080c43ea5bc0d8822017
Syeda Ume Abiha Abidi <batoolabiha84@gmail.com>
2026-08-30 04:29:50 -0700

    DSA Day 24

---
+++ DSA/1929 - concatenation of array.cpp
@@ -0,0 +1,54 @@
+Given an integer array nums of length n, you want to create an array ans of length 2n where 
+ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+
+ 
+Example 1:
+
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+
+
+
+                  CODE (APPROACH 1)
+
+    class Solution {
+      public:
+    vector<int> getConcatenation(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> ans(2 * n);
+
+        for (int i = 0; i < n; i++) {
+              ans [i + n] = nums[i];
+              ans[i] = nums[i];
+        }
+     return ans;
+    }
+};
+
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+
+                  APPROACH 2
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+      int n = nums.size();
+      for (int i=0; i < n; i++) {
+        nums.push_back(nums[i]);
+      }
+     return nums;
+    }
+};
+
+Time Complexity: O(n) amortized
+Auxiliary Space: O(1)
